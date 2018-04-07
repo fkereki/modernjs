1fd98ed5c5c30408384a365005261b27a5db9d07
fkereki <fkereki@linux.fklaptop>
2018-04-07 13:54:46 -0300

    Node examples, and minor fixes

--- chapter03/src/doroundmath.js
+++ chapter03/src/doroundmath.js
@@ -8,4 +8,4 @@ console.log(RM.addR(12.348, 4.221)); // 16.57
 console.log(multR(22.9, 12.4)); // 283.96
 console.log(divR(22, 7)); // 3.14
 
-//   console.log(RM.changeSign(0.07)); // error; RM.changeSign is not a function
+// console.log(RM.changeSign(0.07)); // error; RM.changeSign is not a function
--- chapter03/src/roundmath.js
+++ chapter03/src/roundmath.js
@@ -1,13 +1,17 @@
 /* @flow */
 "use strict";
 
+// These won't be exported:
+
 const roundToCents = (x: number): number => Math.round(x * 100) / 100;
 
 const changeSign = (x: number): number => -x;
 
+// The following will be exported:
+
 const addR = (x: number, y: number): number => roundToCents(x + y);
 
-const subR = (x: number, y: number) => addR(x, changeSign(y));
+const subR = (x: number, y: number): number => addR(x, changeSign(y));
 
 const multR = (x: number, y: number): number => roundToCents(x * y);
 
