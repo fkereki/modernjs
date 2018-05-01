6a758c1f251ab3cf267eac484d4f48253bd49c9a
Federico Kereki <fkereki@gmail.com>
2018-04-30 22:42:23 -0300

    Small changes

--- chapter03/src/process_fork.js
+++ chapter03/src/process_fork.js
@@ -3,13 +3,9 @@
 const path = require("path");
 const { fork } = require("child_process");
 
-console.log(path.resolve("out/process_dir.js"));
-
 const child = fork(path.resolve("out/process_fork_dir.js"));
 
-const filePath = "/home/fkereki";
-
-child.send({ path: filePath });
+child.send({ path: "/home/fkereki" });
 
 child.on("message", data => {
     console.log(String(data));
