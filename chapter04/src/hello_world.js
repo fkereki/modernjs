b7eacc08e0e275e7652368e6e698c2e47e468471
fkereki <fkereki@linux.fklaptop>
2018-05-14 23:19:12 -0400

    Secure server

--- chapter04/src/hello_world.js
+++ chapter04/src/hello_world.js
@@ -6,4 +6,8 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Server alive, with Express!"));
 
-app.listen(8080, () => console.log("Mini server (with Express) ready at http://localhost:8080/!"));
+app.listen(8080, () =>
+    console.log(
+        "Mini server (with Express) ready at http://localhost:8080/!"
+    )
+);
