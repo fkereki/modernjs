f59aacc8f86ee6b0d843f6fcc1bbf8a8e32c16f8
Federico Kereki <fkereki@gmail.com>
2018-06-21 23:51:26 -0300

    Almost ready demo

--- chapter06/src/countryFilterBar/index.js
+++ chapter06/src/countryFilterBar/index.js
@@ -1,7 +1,28 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 export class CountryFilterBar extends React.PureComponent {
+    static propTypes = {
+        list: PropTypes.arrayOf(PropTypes.object).isRequired,
+        onSelect: PropTypes.func.isRequired
+    };
+
+    onSelect(e) {
+        this.props.onSelect(e.target.value);
+    }
+
     render() {
-        return <div style={{ border: "solid" }}>SOMETHING</div>;
+        return (
+            <div>
+                Country:&nbsp;
+                <select onChange={this.onSelect.bind(this)}>
+                    <option value="">Select a country:</option>
+                    {this.props.list.map(x => (
+                        <option key={x.code} value={x.code}>
+                            {x.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        );
     }
 }
--- chapter06/src/resultsDataTable/index.js
+++ chapter06/src/resultsDataTable/index.js
@@ -1,7 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export class ResultsDataTable extends React.PureComponent {
+    static propTypes = {
+        results: PropTypes.arrayOf(PropTypes.object).isRequired
+    };
+
     render() {
-        return <div style={{ border: "solid" }}>RESULTS</div>;
+        if (this.props.results.length === 0) {
+            return "No regions can be shown.";
+        } else {
+            return (
+                <div>
+                    {this.props.results.map(x => (
+                        <div key={x.id}>
+                            {x.name}
+                            {x.cities}
+                            {x.pop}
+                        </div>
+                    ))}
+                </div>
+            );
+        }
     }
 }
