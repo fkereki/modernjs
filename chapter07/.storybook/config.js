a1a45b06177160fe8c4e4f85689447ee2688be25
Federico Kereki <fkereki@gmail.com>
2018-07-01 23:06:04 -0300

    Eject experiment

---
+++ chapter07/.storybook/config.js
@@ -0,0 +1,6 @@
+import { configure } from "@storybook/react";
+
+configure(() => {
+    const req = require.context("../src", true, /\.story\.js$/);
+    req.keys().forEach(filename => req(filename));
+}, module);
