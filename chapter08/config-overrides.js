3ccce4caa17b4803de3ccfb2a03e31157b1a465f
Federico Kereki <fkereki@gmail.com>
2018-07-19 23:50:05 -0300

    Chapter 8, created

--- chapter08/config-overrides.js
+++ chapter08/config-overrides.js
@@ -1,5 +1,4 @@
 const rewireEslint = require("react-app-rewire-eslint");
-const rewireSass = require("react-app-rewire-scss");
 
 function overrideEslintOptions(options) {
     // do stuff with the eslint options...
@@ -9,6 +8,5 @@ function overrideEslintOptions(options) {
 /* global module */
 module.exports = function override(config, env) {
     config = rewireEslint(config, env, overrideEslintOptions);
-    config = rewireSass(config, env);
     return config;
 };
