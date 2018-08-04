76977ee2a25f733a8cf9d80475bb9b68191b0514
Federico Kereki <fkereki@gmail.com>
2018-08-04 12:10:15 -0300

    Debugging finished

--- chapter09/src/counterApp/store.js
+++ chapter09/src/counterApp/store.js
@@ -2,9 +2,26 @@
 
 import { createStore, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import { reducer } from "./counter.reducer.js";
 
-const logger = createLogger({ diff: true, duration: true });
+const logger = createLogger({
+    diff: true,
+    duration: true
+});
 
-export const store = createStore(reducer, applyMiddleware(logger));
+// The following uses only redux-logger
+/*
+export const store =
+    process.env.NODE_ENV === "development"
+        ? createStore(reducer, applyMiddleware(logger))
+        : createStore(reducer);
+*/
+
+// The following uses redux-devtools-extension in addition to redux-logger
+
+export const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(logger))
+);
