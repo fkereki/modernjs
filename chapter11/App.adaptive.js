c4f6b0b6293a869b1297680b8ad893e7eb402195
Federico Kereki <fkereki@gmail.com>
2018-09-05 23:21:48 -0300

    Styled app

--- chapter11/App.adaptive.js
+++ chapter11/App.adaptive.js
@@ -3,14 +3,14 @@
 import React from "react";
 import { Provider } from "react-redux";
 
-import { store } from "./src/regionsStyledApp/store";
-import { ConnectedMain } from "./src/regionsStyledApp/main.connected";
+import { store } from "./src/adaptiveApp/store";
+import { Main } from "./src/adaptiveApp/main";
 
 export default class App extends React.PureComponent<> {
     render() {
         return (
             <Provider store={store}>
-                <ConnectedMain />
+                <Main />
             </Provider>
         );
     }
